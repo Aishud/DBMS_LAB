cedff5813e3306073af045266776a8481d0da951
Aishud <77443206+Aishud@users.noreply.github.com>
2022-12-16 21:23:04 +0530

    Create History_trigger.sql

---
+++ Exp12/History_trigger.sql
@@ -0,0 +1,22 @@
+use library;
+create table historytable like member;
+alter table historytable drop primary key;
+desc historytable;
+/*
++--------------+-------------+------+-----+---------+-------+
+| Field        | Type        | Null | Key | Default | Extra |
++--------------+-------------+------+-----+---------+-------+
+| member_id    | int(10)     | NO   |     | NULL    |       |
+| name         | varchar(20) | YES  |     | NULL    |       |
+| branch_code  | int(10)     | YES  |     | NULL    |       |
+| roll_no      | int(10)     | YES  |     | NULL    |       |
+| email        | varchar(20) | YES  |     | NULL    |       |
+| date_of_join | date        | YES  |     | NULL    |       |
+| status       | varchar(20) | YES  |     | NULL    |       |
++--------------+-------------+------+-----+---------+-------+
+7 rows in set (0.01 sec)
+*/
+CREATE TRIGGER t_up BEFORE UPDATE  ON member 
+FOR EACH ROW  
+ INSERT INTO historytable VALUES(OLD.Member_id,OLD.Name,OLD.Branch_code,OLD.Roll_no,OLD.Email,OLD.Date_of_join,OLD.Status);
+END $
